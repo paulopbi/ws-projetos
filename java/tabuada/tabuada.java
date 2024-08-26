c758751be1e10a7e49309c55e86c498ec5dd3497
Paulo Victor <paulpbi16@gmail.com>
2024-08-26 00:29:29 -0300

    exercicios em java

---
+++ java/tabuada/tabuada.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        int parcela = 4;
+
+        System.out.printf("\n==================\n");
+        System.out.print("\nAlgoritimo de tabuada\n");
+
+        for (int i = 0; i <= 10; i++) {
+            System.out.printf("\n" + parcela + "X" + i + "=" + parcela * i);
+        }
+
+        System.out.printf("\n==================\n");
+    }
+}
